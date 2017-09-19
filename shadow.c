d37fde7ef63884eab0070002e15c83c06d560ead
Sir Anon Robot <siranonrobot@protonmail.com>
2017-09-19 13:25:17 +0200

    account library added

--- shadow.c
+++ shadow.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <time.h>
 
+#include "account.h"
 #include "display.h"
 
 int main(int argc, char const *argv[]) {
@@ -27,29 +28,9 @@ int main(int argc, char const *argv[]) {
     scanf("\n %s", anwser);
 
     if (strcmp(anwser, "new") == 0) {
-      char * username = malloc(20);
-      char * name = malloc(20);
-      char * email = malloc(20);
-      printf("Welcome to Shadow Terminal ! \nPlease anwser the fill : \nUsername (name where we can contact you) ? @");
-      scanf("\n %s", username);
-      printf("Name (Display name) ? ");
-      scanf("\n %s", name);
-      printf("Email (for password recovery) ? ");
-      scanf("\n %s", email);
-      char * password = getpass("Password ? ");
-      printf("%s\n", password);
-      char * password2 = getpass("Again : ");
-      printf("%s\n", password2);
-      if (strcmp(password, password2) == 0) {
-        printf("\n\nPlease Check this information bellow :\n");
-        printf("@%s | %s | %s \n%s | %s", username,name,email,password,password2);
-      } else if (strcmp(password, password2) != 0) {
-        printf("Password Doesnt Match !");
-      }
-
-
+      NewAccount();
     } else if (strcmp(anwser, "login") == 0) {
-      /* code */
+
     }
   }
 
